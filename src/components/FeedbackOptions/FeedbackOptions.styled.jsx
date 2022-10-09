3b419127fcd046ff3943b905668b3580312fa6ff
Alexey Brazhnik <alexey.brazhnyk@gmail.com>
2022-10-09 22:08:45 +0300

    исправлен ключ темы radii

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -10,7 +10,7 @@ export const OptionBtn = styled.button`
   padding: ${({ theme }) => theme.space[3]}px;
   background-color: ${({ theme }) => theme.colors.background};
   border: ${({ theme }) => theme.borders.normal};
-  border-radius: ${({ theme }) => theme.radius.normal};
+  border-radius: ${({ theme }) => theme.radii.normal};
   text-transform: capitalize;
   font-weight: ${({ theme }) => theme.fontWeights.bold};
   cursor: pointer;
